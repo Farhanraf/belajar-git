2545d9d7ef41fc378840e4675573f667cdaa5c11
farhanrafi <farhan.ff547@gmail.com>
2020-08-09 23:15:48 +0700

    first commit

---
+++ src/main/kotlin/Hellokotlin.kt
@@ -0,0 +1,11 @@
+fun main () {
+    val name = "Farhan Rafi"
+    val hobi = "Main game dan Main Sepakbola"
+
+    print("Halo nama saya   :")
+    println(name)
+    print("Hobi saya adalah :")
+    println(hobi)
+    print(if (true)"always true" else "always false")
+
+}
--- src/main/kotlin/app.kt
+++
@@ -1,9 +0,0 @@
-/*
-Nama    : Farhan Rafi Ramadhan
-Kelas   : XI RPL 2
-No      : 11
-
- */
-fun main() {
-    println("Hello kotlin")
-}
